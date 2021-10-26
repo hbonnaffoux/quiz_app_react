58426d4ae69bf5a41e525dc20c7e3850bed2fced
Herve Bonnaffoux <bonnaffouxherve@gmail.com>
2021-10-26 11:19:46 +0200

    parsing and render questions + answers with basic css

--- quiz_app/src/App.js
+++ quiz_app/src/App.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+
+const API_URL =
+  "https://opentdb.com/api.php?amount=10&category=12&difficulty=easy&type=multiple";
 
 function App() {
-  return (
-   <h1>Hello peeps</h1>
+  const[questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+      fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setQuestions(data.results); 
+      });
+  }, []);
+
+  return questions.length > 0 ? (
+    <div className="container">
+      <div className="bg-white text-indigo-800 p-10 rounded-lg shadow-md">
+        <h2 className="text-2xl" dangerouslySetInnerHTML={{__html:questions[0].question}}></h2>
+      </div>
+      <div className="grid grid-cols-2 gap-6 mt-6">
+        <button className="bg-white p-4 text-indigo-800 font-semibold rounded shadow">
+          {questions[0].correct_answer}
+        </button>
+        <button className="bg-white p-4 text-indigo-800 font-semibold rounded shadow">
+          {questions[0].incorrect_answers[0]}
+        </button>
+        <button className="bg-white  p-4 text-indigo-800 font-semibold rounded shadow">
+        {questions[0].incorrect_answers[1]}
+        </button>
+        <button className="bg-white p-4 text-indigo-800 font-semibold rounded shadow">
+        {questions[0].incorrect_answers[2]}
+        </button>
+      </div>
+    </div>
+    ) : (
+      <h1>Loading...</h1>
   );
 }
 
