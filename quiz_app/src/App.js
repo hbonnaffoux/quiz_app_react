ae5012ad9aa8451666bf63c2f62a770ca6a6792b
Herve Bonnaffoux <bonnaffouxherve@gmail.com>
2021-10-27 14:26:52 +0200

    shuffled questions and correct answers

--- quiz_app/src/App.js
+++ quiz_app/src/App.js
@@ -6,22 +6,37 @@ const API_URL =
   "https://opentdb.com/api.php?amount=10&category=12&difficulty=easy&type=multiple";
 
 function App() {
-  const[questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(undefined)
 
   useEffect(() => {
-      fetch(API_URL)
+    fetch(API_URL)
       .then((res) => res.json())
       .then((data) => {
-        setQuestions(data.results); 
+        setQuestions(data.results);
+        setCurrentQuestion(data.results[0])
       });
   }, []);
 
+  const handleAnswer = (answer) => {
+      // check for the answer
+
+      //show another question
+
+      //change score if correct
+  };
+
   return questions.length > 0 ? (
     <div className="container">
-      <Questionaire data={questions[0]}></Questionaire>
+      {currentQuestion && (
+      <Questionaire
+        data={currentQuestion}
+        handleAnswer={handleAnswer}
+      ></Questionaire>
+      )}
     </div>
-    ) : (
-      <h1 className="font-bold text-xl">Loading...</h1>
+  ) : (
+    <h1 className="font-bold text-xl">Loading...</h1>
   );
 }
 
--- quiz_app/src/components/Questionaire.js
+++ quiz_app/src/components/Questionaire.js
@@ -1,30 +1,35 @@
 import React from "react";
 
 const Questionaire = ({
+  handleAnswer,
   data: { question, correct_answer, incorrect_answers },
-}) => (
-  <div>
-    <div className="bg-white text-indigo-800 p-10 rounded-lg shadow-md">
-      <h2
-        className="text-2xl"
-        dangerouslySetInnerHTML={{ __html: question }}
-      ></h2>
+}) => {
+  const shuffledAnswers = [correct_answer, ...incorrect_answers].sort(
+    () => Math.random() - 0.5
+  );
+
+  return (
+    <div>
+      <div className="bg-white text-indigo-800 p-10 rounded-lg shadow-md">
+        <h2
+          className="text-2xl"
+          dangerouslySetInnerHTML={{ __html: question }}
+        ></h2>
+      </div>
+      <div className="grid grid-cols-2 gap-6 mt-6">
+        {shuffledAnswers.map((answer) => (
+          <button
+            className={`${
+              correct_answer === answer ? `bg-purple-300` : `bg-white`
+            } p-4 text-indigo-800 font-semibold rounded shadow`}
+            onClick={() => handleAnswer(answer)}
+          >
+            {answer}
+          </button>
+        ))}
+      </div>
     </div>
-    <div className="grid grid-cols-2 gap-6 mt-6">
-      <button className="bg-white p-4 text-indigo-800 font-semibold rounded shadow">
-        {correct_answer}
-      </button>
-      <button className="bg-white p-4 text-indigo-800 font-semibold rounded shadow">
-        {incorrect_answers[0]}
-      </button>
-      <button className="bg-white  p-4 text-indigo-800 font-semibold rounded shadow">
-        {incorrect_answers[1]}
-      </button>
-      <button className="bg-white p-4 text-indigo-800 font-semibold rounded shadow">
-        {incorrect_answers[2]}
-      </button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Questionaire;
